ed53711c3c72561fa79fae42b7baf90ff5ffd3c4
Prajal Maharjan <91400741+prajall@users.noreply.github.com>
2022-08-11 12:52:39 +0545

    Update movingCircle.cpp

--- movingCircle.cpp
+++ movingCircle.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-int gd=DETECT, gm;
-initgraph(&gd,&gm,"C:/TC/BGI");
+  int gd=DETECT, gm;
+  initgraph(&gd,&gm,"C:/TC/BGI");
 
-for(int i=100; i<540; i++)
-{
-circle(i,200,100);
-delay(10);
-cleardevice();
-}
+   for(int i=100; i<540; i++)
+   {
+      circle(i,200,100);
+      delay(10);
+      cleardevice();
+   }
 
-getch();
-closegraph();
+    getch();
+    closegraph();
 }
