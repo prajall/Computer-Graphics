80359659f73b981ad52a949aa294dc4ada698f27
Prajal Maharjan <91400741+prajall@users.noreply.github.com>
2022-08-11 12:51:29 +0545

    Create movingCircle.cpp

---
+++ movingCircle.cpp
@@ -0,0 +1,19 @@
+#include<graphics.h>
+#include<dos.h>
+#include<conio.h>
+
+int main()
+{
+int gd=DETECT, gm;
+initgraph(&gd,&gm,"C:/TC/BGI");
+
+for(int i=100; i<540; i++)
+{
+circle(i,200,100);
+delay(10);
+cleardevice();
+}
+
+getch();
+closegraph();
+}
